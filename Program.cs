980c291361f100994d4f7c1837a638eaeb00af62
karunakargoud <karunakargoud1116@gmail.com>
2024-12-03 21:02:47 +0530

    FirstCommit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int n=int.Parse(Console.ReadLine());
+            for(int i = 1; i<=n; i++)
+            {
+               for(int j = 1; j <=i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+
+            for(int i = n; i>=1; i--)
+            {
+                for(int j =i ; j>=1 ; j--)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
